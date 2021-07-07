b2a722f61d212e9381cff224aee361cdfb21f05c
9631kunn <infourbh+1@gmail.com>
2021-07-07 16:37:50 +0900

    シェア機能作成

--- App.tsx
+++ App.tsx
@@ -1,16 +1,61 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { useState } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 // import logo from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
 
 export default function App() {
+  type imageType = null | { localUri: string };
+  const [selectedImage, setSelectedImage] = useState<imageType>(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    // 写真ライブラリへのアクセスが拒否された場合
+    if (permissionResult.granted === false) {
+      alert("カメラロールへのアクセス許可は必須です;;");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    // 写真ライブラリへの選択中にキャンセルされた場合
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert("お使いのデバイスではシェアリングが有効ではありません;;");
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>この写真をシェアしよう!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
 
       <Text style={styles.instructions}>HELLO EXPO ^-^</Text>
 
-      <TouchableOpacity onPress={() => alert("Hello Expo Again!")} style={styles.button}>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
@@ -43,5 +88,10 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 20,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
