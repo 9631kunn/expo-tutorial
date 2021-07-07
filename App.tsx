89beac9b690f46d01fb48665812ed00119fec896
9631kunn <infourbh+1@gmail.com>
2021-07-07 14:13:45 +0900

    画像挿入

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+// import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+
       <Text style={{ color: "tomato", fontSize: 30 }}>HELLO EXPO ^-^</Text>
       <StatusBar style="auto" />
     </View>
