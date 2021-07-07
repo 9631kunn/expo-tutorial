6142cf83827bc1e38627a793212a4df2f6cb80f2
9631kunn <infourbh+1@gmail.com>
2021-07-07 15:59:09 +0900

    ボタンの作成とイメージピッカー導入

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 // import logo from "./assets/logo.png";
 
 export default function App() {
@@ -9,6 +9,10 @@ export default function App() {
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
 
       <Text style={styles.instructions}>HELLO EXPO ^-^</Text>
+
+      <TouchableOpacity onPress={() => alert("Hello Expo Again!")} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,5 +34,14 @@ const styles = StyleSheet.create({
     color: "#888",
     fontSize: 18,
     marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
   },
 });
